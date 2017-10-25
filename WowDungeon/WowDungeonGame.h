fbe60ead686808f4204df78372376f543dbf3ee5
David M Allen <dseftu@gmail.com>
2017-10-24 20:09:29 -0400

    Initial project setup

---
+++ WowDungeon/WowDungeonGame.h
@@ -0,0 +1,31 @@
+#pragma once
+#include "Game.h"
+#include "Rectangle.h"
+
+namespace Library
+{
+	class KeyboardComponent;
+}
+
+namespace WowDungeon
+{
+
+	class WowDungeonGame : public Library::Game
+	{
+	public:
+		WowDungeonGame(std::function<void*()> getWindowCallback, std::function<void(SIZE&)> getRenderTargetSizeCallback);
+
+		virtual void Initialize() override;
+		virtual void Shutdown() override;
+		virtual void Update(const Library::GameTime& gameTime) override;
+		virtual void Draw(const Library::GameTime& gameTime) override;
+
+	private:
+		void Exit();
+
+		static DirectX::XMVECTORF32 BackgroundColor;
+
+		std::shared_ptr<Library::KeyboardComponent> mKeyboard;
+		
+	};
+}
